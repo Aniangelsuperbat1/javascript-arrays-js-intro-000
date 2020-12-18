498228a3f12a3fd2f30bbdc047f41d37392f5848
Patrick Zhen <zhen.patrick@gmail.com>
2020-12-18 14:15:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,21 @@
 var chocolateBars = ["snickers", "hundred grad", "kitkat", "skittles" ]
 
 function addElementToBeginningOfArray(array, element){
-
+  return array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   return array.unshift(element)
   return array.push(element)
 
+}
+
+function addElementToEndOfArray(array, element){
+  
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  
 }
 
 function accessElementInArray(array, index){
