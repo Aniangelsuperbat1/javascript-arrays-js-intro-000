285b6a1820371dabf305138ce7c54747a5869a2e
Patrick Zhen <zhen.patrick@gmail.com>
2020-12-18 15:12:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,7 +40,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array
 }
 
-function removeElementFromEndOfArray(){
+function removeElementFromEndOfArray(array){
   array.slice(0, array.length -1 )
   return array
 }
