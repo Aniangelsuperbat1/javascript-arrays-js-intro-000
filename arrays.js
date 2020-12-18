fba35bed3b2a5000ea20bedd61b9d920127f0a7b
Patrick Zhen <zhen.patrick@gmail.com>
2020-12-18 14:27:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -12,7 +12,8 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-
+  array.push(element)
+  return array
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
